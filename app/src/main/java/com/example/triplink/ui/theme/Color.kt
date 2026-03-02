c686e22d78e0f4b3cc757f44a250241058c77e3f
JUANMANUELUQ <126833069+JUANMANUELUQ@users.noreply.github.com>
2026-03-02 08:46:46 -0500

    Colors defined

--- app/src/main/java/com/example/triplink/ui/theme/Color.kt
+++ app/src/main/java/com/example/triplink/ui/theme/Color.kt
@@ -2,6 +2,8 @@ package com.example.triplink.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+//Colores de ejemplo
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
@@ -9,3 +11,33 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+//Colores de la aplicacion
+
+val PrincipalRed = Color(0xFFB3261E)
+
+val PrincipalOrange = Color(0xFFF57F17)
+
+val PastelOrange = Color(0xFFFFE082)
+
+val PrincipalGreen = Color(0xFF3EB31E)
+
+val DarkGreen = Color(0xFF2E7D32)
+
+val PrincipalBlue = Color(0xFF2563EB)
+
+val PrincipalBlueBlocked = Color(0xB22563EB)
+
+val PastelBlue = Color(0xFFBBDEFB)
+
+val Purple = Color(0xFF6A1B9A)
+
+val PastelPurple = Color(0xFFF3E5F5)
+
+val LightGary = Color(0xFFF8FAFC)
+
+val PrincipalGray = Color(0xFFAEADAD)
+
+val DarkGray = Color(0xFF6B7B90)
+
+
